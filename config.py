fa2d45fda81736bf822c7937b59ea10d3fabafc5
Abu Danish <abudanish32@gmail.com>
2026-05-24 23:34:37 +0200

    Add project configuration and hyperparamets

---
+++ config.py
@@ -0,0 +1,10 @@
+config = {
+    
+    "batch_size" : 64, 
+    "learning_rate": 0.001,
+    "num_epochs": 10,
+    "num_classes": 10,
+    "device": "mps",
+    "data_dir": "./data",
+    "save_path":"/checkpoints/model.pth",
+}
