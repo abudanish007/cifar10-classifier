e12479c960b67fa4c4441c66445e34cd83fa5f50
Abu Danish <abudanish32@gmail.com>
2026-05-27 23:38:19 +0200

    Add model file

---
+++ model.py
@@ -0,0 +1,41 @@
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CNN,self).__init__()
+        
+        self.features = nn.Sequential(
+            
+            #block 1
+            nn.Conv2d(3,32,kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLu(),
+            nn.Conv2d(32,32,kernel_size = 3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLu(),
+            nn.MaxPool2d(2,2),
+            nn.Dropout(0.25),
+            
+            #block 2
+            nn.Conv2d(32,64,kernel_size=3, padding=1),
+            nn.BatchNor2d(64),
+            nn.ReLu(),
+            nn.Conv2d(64,64,kernel_size = 3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLu(),
+            nn.MaxPool2d(2,2),
+            nn.Dropout(0.25),
+        )
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(64*8*8, 512),
+            nn.ReLu(),
+            nn.Dropout(0.5),
+            nn.Linear(512, num_classes)
+        )
+        
+    def forward(self,x):
+        x = self.features(x)
+        x = x.view(x.size(0),-1)
+        x = self.classifier(x)
+        return x
