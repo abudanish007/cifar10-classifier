752b2dadf094f2bd8350c157ffec77e9b6353ee0
Abu Danish <abudanish32@gmail.com>
2026-05-31 22:18:47 +0200

    Added the model evaluator

---
+++ evaluator.py
@@ -0,0 +1,28 @@
+import torch
+from tqdm import tqdm
+
+class Evaluator:
+    def __init__(self, model, criterion, device):
+        self.model = model
+        self.criterion = criterion
+        self.deice = device 
+        
+    def evaluate(self, test_loader):
+        self.model.eval()
+        total_loss = 0 
+        correct = 0
+        total = 0
+        
+        with torch.no_grad():
+            for inputs, labels in tqdm(test_loader, desc= "Evaluating"):
+                inputs, labels = inputs.to(self.deice), labels.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs,labels)
+                
+                
+                total_loss += loss.item()
+                _, predicted = outputs.max(1)
+                correct += predicted.eq(labels).sum().item()
+                total += labels.size(0)
+                
+        return total_loss / len(test_loader), 100. * correct/total
